47a5fb7b31ce3799225447e2681afcedd2035644
mats wigle <mats.wigle@basefarm.com>
2020-06-04 10:27:18 +0200

    added link and more output

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ the thought is to create a Raspberry pi weather station with powershell. Lots of
 
 Based on this project (not done by me)
 https://projects.raspberrypi.org/en/projects/build-your-own-weather-station
+
+## Links
+https://docs.microsoft.com/sv-se/azure/iot-hub/iot-hub-raspberry-pi-kit-node-get-started
--- weatherbox.ps1
+++ weatherbox.ps1
@@ -2,13 +2,15 @@
 
 $stepSeconds = 15
 $continue = $true
+$scriptversion = "0.0.1"
 
 while ($continue) {
 
     Write-Output "Start of loop: $(Get-Date)"
+    Write-Output "Script version: $scriptversion"
     Write-Output "Hostname: $(hostname)"
     Write-Output "Wait: $stepSeconds sec"
     Write-Output "End loop $(Get-Date)"
     Start-Sleep -Seconds $stepSeconds
-    
+
 }
