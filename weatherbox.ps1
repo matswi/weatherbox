c6a6472a28a395dcde7a72becf5ae38403276fa6
mats wigle <mats.wigle@basefarm.com>
2020-06-04 14:41:29 +0200

    added pwsh version

--- weatherbox.ps1
+++ weatherbox.ps1
@@ -4,9 +4,12 @@ $stepSeconds = 15
 $continue = $true
 $scriptversion = "0.0.6"
 
+$pswhVersion = $PSVersionTable.psversion.ToString()
+
 while ($continue) {
 
     Write-Output "Start of loop: $(Get-Date)"
+    Write-Output "Powershell version: $pswhVersion"
     Write-Output "Script version: $scriptversion"
     Write-Output "Hostname: $(hostname)"
     Write-Output "Wait: $stepSeconds sec"
